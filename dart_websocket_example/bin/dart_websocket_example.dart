4a566d92546a5b944639e566cec1bdd550e4ea35
Curt Cox <curtcox@gmail.com>
2022-10-12 07:32:42 -0500

    dart --version Dart SDK version: 2.18.2 (stable) (Tue Sep 27 13:24:11 2022 +0200) on "macos_arm64" dart create dart_websocket_example

---
+++ dart_websocket_example/bin/dart_websocket_example.dart
@@ -0,0 +1,5 @@
+import 'package:dart_websocket_example/dart_websocket_example.dart' as dart_websocket_example;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_websocket_example.calculate()}!');
+}
---
+++ dart_websocket_example/test/dart_websocket_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_websocket_example/dart_websocket_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
